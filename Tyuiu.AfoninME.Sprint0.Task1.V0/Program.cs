bee45d977f77f14a66f5409277a82fb9dd95ef91
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-09-13 17:19:42 +0500

    Task1

---
+++ Tyuiu.AfoninME.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AfoninME.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Maksim";
+            int age = 18;
+            Console.WriteLine("Hello, my name is " + name + " ,i am " + age + " years old!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
